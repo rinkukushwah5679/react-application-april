ba14475ba0c40ef8e1fddf1a7fd687f307bf0d45
Arvind Kushwah <arvindkushwah9@gmail.com>
2024-04-05 12:02:49 +0530

    Added lowercases button and added text summary

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,18 +6,29 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     };
+    const handleLoClick = ()=>{
+        let newText = text.toLowerCase();
+        setText(newText);
+    };
     const HandleonChange = (event)=>{
         // console.log("Handel on change")
         setText(event.target.value)
     };
     const [text, setText] = useState("Enter text here");
   return (
-    <div>
+    <>
+    <div className='container'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
           <textarea className="form-control" value={text} placeholder={text} onChange={HandleonChange} id="myBox" rows="8"></textarea>
         </div>
-        <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+        <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
     </div>
+    <div className="container my-3">
+        <h1>You text summary</h1>
+        <p>{text.split(" ").length} words and {text.length} characters</p>
+    </div>
+    </>
   )
 }
