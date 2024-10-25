5831ecf326336b36911e07cc34e2ed595dc2acf8
Arvind Kushwah <arvindkushwah9@gmail.com>
2024-10-25 11:50:41 +0530

    Updates

---
+++ precache-manifest.72c85856c0b359b801148744cf4678f5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c694176f14112b4d4fd41c88aa9806",
+    "url": "/react-application-april/index.html"
+  },
+  {
+    "revision": "3e511d0eefc5dd4d8482",
+    "url": "/react-application-april/static/css/main.44f5a7bf.chunk.css"
+  },
+  {
+    "revision": "7d64c3fedebfc2bbb303",
+    "url": "/react-application-april/static/js/2.13ce7395.chunk.js"
+  },
+  {
+    "revision": "dcd114deef03e1326d0dadab39a6fdac",
+    "url": "/react-application-april/static/js/2.13ce7395.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e61dffbe248831df8ad",
+    "url": "/react-application-april/static/js/3.11f333aa.chunk.js"
+  },
+  {
+    "revision": "3e511d0eefc5dd4d8482",
+    "url": "/react-application-april/static/js/main.13793245.chunk.js"
+  },
+  {
+    "revision": "57161cf805c8c1ab2911",
+    "url": "/react-application-april/static/js/runtime-main.87f42e8b.js"
+  },
+  {
+    "revision": "12b1fefd7b46e3fc1c4c24d9bd963a2b",
+    "url": "/react-application-april/static/media/default_bg.12b1fefd.jpeg"
+  },
+  {
+    "revision": "d45dce5583ff8007b332b7053dcaf592",
+    "url": "/react-application-april/static/media/dog.d45dce55.jpg"
+  }
+]);
