a6fe4849e3c4342ed659f2863efdd49f01fdd2f5
Arvind Kushwah <arvindkushwah9@gmail.com>
2024-10-25 11:48:46 +0530

    sdc

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "https://65e6-2401-4900-1f39-23c4-b785-b939-c8c3-5294.ngrok-free.app/"
+export const BASE_URL = "https://7d81-103-219-229-188.ngrok-free.app/"
 // const user_data = localStorage.getItem('login_user');
 // const current_user = JSON.parse(user_data);
 
